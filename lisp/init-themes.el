3e2723a6ade62b523b320858d33c0c836d7328dd
Qingyi <qingyi.tss@gmail.com>
2021-01-22 00:53:43 +0800

    Use all-the-icons for modeline

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,18 +1,21 @@
 ;;; init-themes.el --- Configure emacs color theme -*- lexical-binding: t -*-
 
+;; in order for the icons to work, fonts should be installed
+;; run `M-x all-the-icons-install-fonts` command when first installed
+(use-package all-the-icons)
+
 (use-package doom-modeline
   :ensure t
   :hook (after-init . doom-modeline-mode)
   :config
-  (setq doom-modeline-icon nil))
+  (setq doom-modeline-icon (display-graphic-p)))
 
 (use-package doom-themes
   :ensure t
   :hook (after-init . (lambda () (load-theme 'doom-one t)))
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
 
   (doom-themes-org-config))
 
