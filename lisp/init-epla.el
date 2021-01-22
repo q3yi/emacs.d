957b14c9dbcb06ca30ea17dcde867e110d76455f
Qingyi <qingyi.tss@gmail.com>
2021-01-22 17:05:31 +0800

    Remove melpa-stable

--- lisp/init-epla.el
+++ lisp/init-epla.el
@@ -7,7 +7,7 @@
 ;; ustc mirror
 (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
+                         ;; ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
                          ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
 
 (require 'package)
