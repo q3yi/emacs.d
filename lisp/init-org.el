14d110319c466f2ded4ea0ce790348264e0eed60
Qingyi <qingyi.tss@gmail.com>
2021-12-14 21:52:22 +0800

    fix incorrect latex preview in org mode with Chinese

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,21 +2,34 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun qy/org-mode-setup ()
+(defun max/org-mode-setup ()
   "Setup config when org mode startup."
   (org-indent-mode)
   (auto-fill-mode t)
   (setq truncate-lines nil))
 
-(defun qy/org-setup-chinese-latex ()
-  "Use xelatex instead of pdflatex to support export org file to pdf."
+(defun max/org-use-xelatex ()
+  "Use xelatex instead of pdflatex to support export org file to pdf and preview latex in org file."
   (require 'ox-latex)
-  ;; use xelatex to support CJK
+  ;; define a new preview process, use xelatex instead of latex
+  (add-to-list 'org-preview-latex-process-alist '(xdvsvgm :progams
+							  ("xelatex" "dvisvgm")
+							  :discription "xdv > svg"
+							  :message "you need install the programs: xelatex and dvisvgm."
+							  :image-input-type "xdv"
+							  :image-output-type "svg"
+							  :image-size-adjust (1.7 . 1.5)
+							  :latex-compiler ("xelatex -interaction nonstopmode -no-pdf -output-directory %o %f")
+							  :image-converter ("dvisvgm %f -n -b min -c %S -o %O")))
+  ;; use svg for preview instead of png
+  ;; may not be worked on system that do not support display svg
+  (setq org-preview-latex-default-process 'xdvsvgm)
+  ;; use xelatex to support CJK when export org file to pdf
   (setq org-latex-compiler "xelatex")
   (setq org-latex-pdf-process '("xelatex %f"))
   (add-to-list 'org-latex-default-packages-alist '("" "ctex" t ("xelatex"))))
 
-(defun qy/org-setup-task-system ()
+(defun max/org-setup-task-system ()
   "Setup my task management system."
   (setq org-directory "~/OrgFiles"
 	org-default-notes-file (expand-file-name "Tasks.org" org-directory)
@@ -43,23 +56,21 @@
 (use-package org
   :demand
   :ensure
-  :hook (org-mode . qy/org-mode-setup)
+  :hook (org-mode . max/org-mode-setup)
   :custom
   ((org-ellipsis " ▾")
-   ;; use svg for preview instead of png
-   ;; may not be worked on system that do not support display svg
-   (org-latex-create-formula-image-program 'dvisvgm)
    (org-return-follows-link t))
 
   :bind
   (("C-c a" . org-agenda)
    ("C-c c" . org-capture)
    :map org-mode-map
-   ("C-c C-q". counsel-org-tag))
+   ("C-c C-q" . counsel-org-tag)
+   ("<s-return>" . org-insert-heading))
 
   :config
-  (qy/org-setup-task-system)
-  (qy/org-setup-chinese-latex)
+  (max/org-setup-task-system)
+  (max/org-use-xelatex)
   (require 'org-tempo))
 
 ;; use org-fragtog to toggle org-mode latex preview
