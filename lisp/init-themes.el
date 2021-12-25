862e0582c438d25dda79b1d777e33b9098bd7467
Qingyi <qingyi.tss@gmail.com>
2021-12-25 15:20:44 +0800

    Auto set themes style by time when emacs start.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -15,16 +15,19 @@
   :ensure
   :init
   (setq modus-themes-italic-constructs t
-        modus-themes-bold-constructs nil
+	modus-themes-bold-constructs nil
 	modus-themes-prompts '(bold intense)
 	modus-themes-mode-line '(accented)
 	modus-themes-paren-match nil
-        modus-themes-region '(bg-only no-extend))
+	modus-themes-region '(bg-only no-extend))
   ;; Load the theme files before enabling a theme
   (modus-themes-load-themes)
   :config
-  ;; Load the theme of your choice:
-  (modus-themes-load-vivendi))
+  ;; Load light or dark theme depend on current hour when open emacs
+  (let ((hour (nth 2 (decode-time (current-time)))))
+    (if (and (> hour 8) (< hour 20))
+	(modus-themes-load-operandi)
+      (modus-themes-load-vivendi))))
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
