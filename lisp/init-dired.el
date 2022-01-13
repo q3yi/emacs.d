67fd1939f06752df6282966485c3cab67e012906
Qingyi <qingyi.tss@gmail.com>
2022-01-13 13:41:18 +0800

    Remove dired-single package, use customed function instead.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -2,19 +2,31 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package dired
-  :ensure nil
-  :config
-  (setq dired-listing-switches "-alh"
-	dired-dwim-target t))
+(require 'dired)
+(require 'dired-x)
 
-(use-package dired-single
-  :after dired
-  :bind
-  (:map dired-mode-map
-	([remap dired-find-file] . dired-single-buffer)
-	([remap dired-up-directory] . dired-single-up-directory)
-	("b" . dired-single-up-directory)))
+(setq dired-listing-switches "-alh")
+(setq dired-dwim-target t)
+
+(defun max/dired-find-file ()
+  "Extend `dired-find-file', so it open directory in current buffer."
+  (interactive)
+  (let ((file-name (dired-get-file-for-visit)))
+    (if (file-directory-p file-name)
+	(progn
+	  (set-buffer-modified-p nil)
+	  (find-alternate-file file-name))
+      (dired--find-possibly-alternative-file file-name))))
+
+(defun max/dired-up-directory ()
+  "Vist parent directory in same buffer."
+  (interactive)
+  (set-buffer-modified-p nil)
+  (find-alternate-file ".."))
+
+(define-key dired-mode-map [remap dired-find-file] 'max/dired-find-file)
+(define-key dired-mode-map [remap dired-up-directory] 'max/dired-up-directory)
+(define-key dired-mode-map (kbd "b") 'max/dired-up-directory)
 
 (use-package all-the-icons-dired
   :after (all-the-icons dired)
@@ -23,6 +35,6 @@
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (provide 'init-dired)
-;;; init-dired ends here
+;;; init-dired.el ends here
 
 
