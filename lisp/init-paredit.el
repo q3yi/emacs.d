8a1481be25a241d28d1e628c7484032ea21ff228
Qingyi <qingyi.tss@gmail.com>
2022-01-16 23:15:38 +0800

    Replace smartparens with paredit and electric-pair.

---
+++ lisp/init-paredit.el
@@ -0,0 +1,36 @@
+;;; init-paredit.el -- Config paredit -*- lexical-binding: t -*-
+;;; Commentary:
+;;;
+;;; Code:
+
+(use-package paredit
+  :delight " Par"
+  :bind
+  (:map paredit-mode-map
+	("M-<up>" . paredit-splice-sexp)
+	("M-<down>" . paredit-wrap-round))
+  :config
+  (define-key paredit-mode-map (kbd "M-s") nil)
+  (define-key paredit-mode-map (kbd "M-?") nil)
+  :hook
+  (emacs-lisp-mode . paredit-mode)
+  (scheme-mode . paredit-mode))
+
+(defun max/turn-off-electric-pair-mode ()
+  "Turn off `electric-pair-mode'."
+  (electric-pair-mode 0))
+
+;; Turn on `electric-pair-mode' on `text-mode' and `prog-mode'
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'text-mode-hook 'electric-pair-mode)
+
+;; Disable `electric-pair-mode' when paredit is enabled
+(add-hook 'paredit-mode-hook 'max/turn-off-electric-pair-mode)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(provide 'init-paredit)
+;;; init-paredit.el ends here
+
+
