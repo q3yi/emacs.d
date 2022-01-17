c98eb01af4e750cc35324ff7058a0802b0bebbd5
Qingyi <qingyi.tss@gmail.com>
2022-01-17 16:47:06 +0800

    Update org directory and org-roam template.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,77 +2,68 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'org)
+(require 'org-tempo)
+(require 'org-habit)
+(require 'ox-latex)
+
 (defun max/org-mode-setup ()
   "Setup config when org mode startup."
   (org-indent-mode)
   (auto-fill-mode t)
   (setq truncate-lines nil))
 
-(defun max/org-use-xelatex ()
-  "Use xelatex instead of pdflatex to support export org file to pdf and preview latex in org file."
-  (require 'ox-latex)
-  ;; define a new preview process, use xelatex instead of latex
-  (add-to-list 'org-preview-latex-process-alist '(xdvsvgm :progams
-							  ("xelatex" "dvisvgm")
-							  :discription "xdv > svg"
-							  :message "you need install the programs: xelatex and dvisvgm."
-							  :image-input-type "xdv"
-							  :image-output-type "svg"
-							  :image-size-adjust (1.7 . 1.5)
-							  :latex-compiler ("xelatex -interaction nonstopmode -no-pdf -output-directory %o %f")
-							  :image-converter ("dvisvgm %f -n -b min -c %S -o %O")))
-  ;; use svg for preview instead of png
-  ;; may not be worked on system that do not support display svg
-  (setq org-preview-latex-default-process 'xdvsvgm)
-  ;; use xelatex to support CJK when export org file to pdf
-  (setq org-latex-compiler "xelatex")
-  (setq org-latex-pdf-process '("xelatex %f"))
-  (add-to-list 'org-latex-default-packages-alist '("" "ctex" t ("xelatex"))))
+(add-hook 'org-mode-hook 'max/org-mode-setup)
 
-(defun max/org-setup-task-system ()
-  "Setup my task management system."
-  (setq org-directory "~/OrgFiles"
-	org-default-notes-file (expand-file-name "Tasks.org" org-directory)
-	org-agenda-files (directory-files org-directory t "\\.org$")
+;; define a new preview process, use xelatex instead of latex
+(add-to-list 'org-preview-latex-process-alist
+	     '(xdvsvgm :progams
+		       ("xelatex" "dvisvgm")
+		       :discription "xdv > svg"
+		       :message "you need install the programs: xelatex and dvisvgm."
+		       :image-input-type "xdv"
+		       :image-output-type "svg"
+		       :image-size-adjust (1.7 . 1.5)
+		       :latex-compiler ("xelatex -interaction nonstopmode -no-pdf -output-directory %o %f")
+		       :image-converter ("dvisvgm %f -n -b min -c %S -o %O")))
 
-	org-refile-targets '(("Archive.org" :maxlevel . 1))
+;; use new svg for preview instead of png
+;; may not be worked on system that do not support display svg
+(setq org-preview-latex-default-process 'xdvsvgm)
 
-	org-agenda-start-with-log-mode t
-	org-log-done 'time
-	org-log-into-drawer t
-	org-export-coding-system 'utf-8)
+;; use xelatex to support CJK when export org file to pdf
+(setq org-latex-compiler "xelatex")
+(setq org-latex-pdf-process '("xelatex %f"))
 
-  (setq org-todo-keywords
-	'((sequence "TODO(t)" "|" "DONE(d!/!)")
-	  (sequence "READING(r!)" "WAIT(w)" "|" "BREAK(b)" "FIN(f!/!)")))
+;; add ctex package to exported latex file
+(add-to-list 'org-latex-default-packages-alist '("" "ctex" t ("xelatex")))
 
-  ;; auto save all org buffer when refile
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+(defvar max/roam-directory "~/Roam")
 
-  (require 'org-habit)
-  (add-to-list 'org-modules 'org-habit)
-  (setq org-habit-graph-column 60))
+(setq org-directory (file-name-concat max/roam-directory "agenda")
+      org-default-notes-file (expand-file-name "tasks.org" org-directory)
+      org-agenda-files (directory-files org-directory t "\\.org$")
+      org-refile-targets '(("archive.org" :maxlevel . 1))
+      org-agenda-start-with-log-mode t
 
-(use-package org
-  :demand
-  :ensure
-  :hook (org-mode . max/org-mode-setup)
-  :pin gnu
-  :custom
-  ((org-ellipsis " ▾")
-   (org-return-follows-link t))
+      org-ellipsis " ▾"
+      org-return-follows-link t
 
-  :bind
-  (("C-c a" . org-agenda)
-   ("C-c c" . org-capture)
-   :map org-mode-map
-   ("C-c C-q" . counsel-org-tag)
-   ("<s-return>" . org-insert-heading))
+      org-log-done 'time
+      org-log-into-drawer t
 
-  :config
-  (max/org-setup-task-system)
-  (max/org-use-xelatex)
-  (require 'org-tempo))
+      org-export-coding-system 'utf-8
+      org-habit-graph-column 60)
+
+;; auto save all org buffer when refile
+(advice-add 'org-refile :after 'org-save-all-org-buffers)
+(add-to-list 'org-modules 'org-habit)
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(define-key org-mode-map (kbd "C-c C-q") 'counsel-org-tag)
+(define-key org-mode-map (kbd "<s-return>") 'org-insert-heading)
 
 ;; use org-fragtog to toggle org-mode latex preview
 (use-package org-fragtog
@@ -82,25 +73,30 @@
 
 ;; export org as hugo compatible markdown
 (use-package ox-hugo
-  :ensure t
-  :after ox)
-
-;; beautify headline bullets
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode))
+  :after ox
+  :hook
+  (org-mode . (lambda ()
+		(with-eval-after-load 'ox
+		  (require 'ox-hugo)))))
 
 (use-package org-roam
-  :ensure t
   :init
-  (setq org-roam-v2-ack t
-	org-roam-directory (file-truename "~/box")
-	org-roam-db-location (file-truename "~/box/roam/org-roam.db"))
+  (setq org-roam-directory max/roam-directory)
+  (setq org-roam-db-location
+	(file-name-concat max/roam-directory ".org-roam" "org-roam.db"))
+  :commands (org-roam-buffer-toggle
+	     org-roam-node-find
+	     org-roam-node-insert)
   :bind
-  (("C-c o l" . org-roam-buffer-toggle)
-   ("C-c o f" . org-roam-node-find)
-   ("C-c o i" . org-roam-node-insert))
-  :config (org-roam-db-autosync-enable))
+  (("C-c r l" . org-roam-buffer-toggle)
+   ("C-c r f" . org-roam-node-find)
+   ("C-c r i" . org-roam-node-insert))
+  :config
+  (setq org-roam-capture-templates
+	'(("d" "default" plain "%?" :target
+	   (file+head "${slug}.org" "#+title: ${title}\n#+created_time: %<%Y-%m-%dT%H:%M:%S%:z>\n")
+	   :unnarrowed t)))
+  (org-roam-db-autosync-enable))
 
 (provide 'init-org)
 ;;; init-org.el ends here
