ae888cb0fc46d26d0c159d7da09fea4ec4854e94
Qingyi <qingyi.tss@gmail.com>
2022-01-17 15:09:01 +0800

    Use GNU ls to change the format of timestamp.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -6,12 +6,18 @@
 (require 'dired-x)
 (require 'ls-lisp)
 
-(setq dired-listing-switches "-alh")
-(setq dired-dwim-target t)
+(setq delete-by-moving-to-trash t)
+(setq dired-kill-when-opening-new-dired-buffer t)
 
-;; Using ls-lisp instead of external ls command
-(setq ls-lisp-use-insert-directory-program nil
-      ls-lisp-dirs-first t)
+;; ATTENTION on macOS
+;; ls on BSD system do not support --group-directories-first or --time-style
+;; consider install GNU coreutils with homebrew
+;; `brew install coreutils`
+(setq dired-listing-switches "-AGlhv --group-directories-first --time-style=long-iso")
+(setq dired-dwim-target t)
+(setq dired-use-ls-dired t)
+
+(setq dired-kill-when-opening-new-dired-buffer t)
 
 (defun max/dired-find-file ()
   "Extend `dired-find-file', so it open directory in current buffer."
@@ -29,9 +35,12 @@
   (set-buffer-modified-p nil)
   (find-alternate-file ".."))
 
-(define-key dired-mode-map [remap dired-find-file] 'max/dired-find-file)
-(define-key dired-mode-map [remap dired-up-directory] 'max/dired-up-directory)
-(define-key dired-mode-map (kbd "b") 'max/dired-up-directory)
+;; (define-key dired-mode-map [remap dired-find-file] 'max/dired-find-file)
+;; (define-key dired-mode-map [remap dired-up-directory] 'max/dired-up-directory)
+(define-key dired-mode-map (kbd "b") 'dired-up-directory)
+
+(add-hook 'dired-mode-hook 'hl-line-mode)
+
 
 (provide 'init-dired)
 ;;; init-dired.el ends here
