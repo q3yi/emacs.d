5514fc82a9e7d2b966dca88ff7fa65dc3102c224
Qingyi <qingyi.tss@gmail.com>
2022-01-23 21:28:48 +0800

    Set curl proxy in elfeed config.

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -12,8 +12,19 @@
 	elfeed-enclosure-default-dir (file-name-concat
 				      elfeed-db-directory
 				      "encloures"))
+  :bind
+  (:map elfeed-search-mode-map
+	;; The original binding "g" . elfeed-update--force somehow
+	;; is not working in my emacs, so I change it to the interactive
+	;; function.
+	("g" . elfeed-update))
   :config
-  (setq elfeed-search-filter "@1-week-ago"))
+  (setq elfeed-search-filter "@1-week-ago")
+  ;; Set proxy for curl
+  (let ((proxy (assoc "http" url-proxy-services)))
+    (when proxy
+      (setq elfeed-curl-extra-arguments
+	    (list "--proxy" (format "http://%s" (cdr proxy)))))))
 
 (provide 'init-elfeed)
 ;;; init-elfeed.el ends here
