b56bd2f1b1c1dc2c2bdf2cdfba40e9fdad77d277
Yi Qing <qingyi.tss@gmail.com>
2023-05-03 15:13:51 +0800

    Save and format solidity buffer.

--- lisp/init-solidity.el
+++ lisp/init-solidity.el
@@ -4,9 +4,20 @@
 
 (require 'init-package-util)
 
+(defun solidity-save-and-format-buffer ()
+  "Format current with forge fmt."
+  (interactive)
+  (save-buffer)
+  (shell-command (format "forge fmt %s" (buffer-file-name)))
+  (revert-buffer nil t)
+  (message "Save and format %s" (buffer-file-name)))
+
 (use-package solidity-mode
   :pin melpa-stable
-  :custom (solidity-solc-path "/usr/local/bin/solcjs"))
+  :custom (solidity-solc-path "/usr/local/bin/solc")
+  :bind (:map solidity-mode-map
+	      ("C-c \\" . solidity-save-and-format-buffer))
+  :hook ((solidity-mode . (lambda () (setq-local c-basic-offset 4)))))
 
 (provide 'init-solidity)
 ;;; init-solidity.el ends here
