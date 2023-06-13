2408c452a32a717a7d280a50d336306243d2dfc7
Yi Qing <qingyi.tss@gmail.com>
2023-06-13 10:26:10 +0800

    add some key-bindings to flymake.

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -6,5 +6,10 @@
 
 (add-hook 'prog-mode-hook #'flymake-mode)
 
+(define-key flymake-mode-map (kbd "C-c 1 l") #'flymake-show-buffer-diagnostics)
+(define-key flymake-mode-map (kbd "C-c 1 L") #'flymake-show-project-diagnostics)
+(define-key flymake-mode-map (kbd "C-c 1 n") #'flymake-goto-next-error)
+(define-key flymake-mode-map (kbd "C-c 1 p") #'flymake-goto-prev-error)
+
 (provide 'init-flymake)
 ;;; init-flymake.el ends here
